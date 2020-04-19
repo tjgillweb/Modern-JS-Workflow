aecd722663c718188bba56ab76f2163367a924ad
tjgillweb <tjgillweb@gmail.com>
2020-04-19 23:26:20 +0530

    Setup File Structure

---
+++ dist/assets/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var displayName = function displayName(name) {
+  console.log("hello ".concat(name));
+};
+
+displayName("John");
+displayName("Elie");
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const displayName = name => {
+    console.log(`hello ${name}`);
+}
+displayName("John");
+displayName("Elie");
